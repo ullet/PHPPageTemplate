90388650c755c386cd741786c3d1e3186ebbbbd8
Trevor Barnett <mr.ullet@gmail.com>
2006-09-03 19:26:44 +0100

    "XML documentation" and copyright
    Added "XML documentation" for core classes (PageBase, PageTemplateBase).
    Added copyright message to all files.

--- PageBase.php
+++ PageBase.php
@@ -1,33 +1,105 @@
 <?php
+/*
+ *************************************************************************
+ * PHPPageTemplate: A PHP4 page templating system.                       *
+ * Version 0.0.0 (03 September 2006)                                     *
+ * Copyright (C) 2006 Trevor Barnett                                     *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the Free Software           *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+ * USA                                                                   *
+ *************************************************************************
+ */
+
 include_once("PageTemplateBase.php");
+
+//* <class name="PageBase" modifiers="public, abstract">
+//* Base class for a web page based on a template
+//* </class>
 class PageBase // abstract
 {
-    var $_pageTemplate;    
+    //// private member variables
+    //* <property name="_pageTemplate" modifiers="private" 
+    //* type="&PageTemplateBase">
+    //* Template for Page
+    //* </property>
+    var $_pageTemplate;   
+    //* <property name="_placeHolderfunctions" modifiers="private"
+    //* type="[Associative Array]">
+    //* Array of functions for place holders
+    //* </property>
     var $_placeHolderfunctions;
+    //** end
     
-    // protected methods
+    //// protected accessors
+    //* <method name="_set_PageTemplate" modifiers="protected"
+    //* returnType="void">
+    //* Sets page template
+    //* <parameter name="$pageTemplate" type="&PageTemplateBase">
+    //* Template for page
+    //* </parameter>
+    //* </method>
+    function _set_PageTemplate(&$pageTemplate)
+    {
+        $this->_pageTemplate =& $pageTemplate;
+    }
+    //// end
+    
+    //// protected methods
+    //* <method name="_RegisterPlaceHolder" modfiers="protected" 
+    //* returnType="void">
+    //* Add function to list of placeholder functions
+    //* <parameter name="$placeHolderName" type="string">
+    //* Name of placeholder
+    //* </parameter>
+    //* <parameter name="$placeHolderName" type="string">
+    //* Name of function to be called to fill place holder
+    //* </parameter>
+    //* </method>
     function _RegisterPlaceHolder($placeHolderName, $functionName)
     {
         $this->_placeHolderfunctions[$placeHolderName] = $functionName;
     }
     
-    function _set_PageTemplate(&$pageTemplate)
-    {
-        $this->_pageTemplate =& $pageTemplate;
-    }
-    
+    //* <method name="_Initialise" modfiers="protected" 
+    //* returnType="void">
+    //* Initialise page
+    //* </method>
     function _Initialise()
     {
         $this->_placeHolderfunctions = array();
     }
+    //// end
     
-    // public methods
+    //// public methods
+    //* <method name="Render" modfiers="public" 
+    //* returnType="void">
+    //* Render page
+    //* </method>
     function Render()
     {
         $this->_Initialise();
         $this->_pageTemplate->Render();       
     }
     
+    //* <method name="GetFunctionForPlaceHolder" modfiers="public" 
+    //* returnType="string">
+    //* Gets function to be called to fill the specified placeholder
+    //* <parameter name="$placeHolderName" type="string">
+    //* Name of placeholder
+    //* </parameter>
+    //* </method>
     function GetFunctionForPlaceHolder($placeHolderName)
     {
         if (!in_array($placeHolderName, 
@@ -37,5 +109,6 @@ class PageBase // abstract
         }
         return $this->_placeHolderfunctions[$placeHolderName];
     }
+    //// end
 }
 ?>
--- PageTemplateBase.php
+++ PageTemplateBase.php
@@ -1,55 +1,138 @@
 <?php
+/*
+ *************************************************************************
+ * PHPPageTemplate: A PHP4 page templating system.                       *
+ * Version 0.0.0 (03 September 2006)                                     *
+ * Copyright (C) 2006 Trevor Barnett                                     *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the Free Software           *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+ * USA                                                                   *
+ *************************************************************************
+ */
+ 
 include_once("PageBase.php");
+
+//* <class name="PageTemplateBase" modifiers="public, abstract">
+//* Base class for a web page template
+//* </class>
 class PageTemplateBase // abstract
 {
-    // private members
-    var $_page; // as PageBase
+    //// private members
+    //* <property name="_page" modifiers="private" 
+    //* type="&PageBase">
+    //* Reference to page using template
+    //* </property>
+    var $_page;
+    //* <property name="_title" modifiers="private" 
+    //* type="string">
+    //* Title of page for display in browser title bar
+    //* </property>
     var $_title = "Untitled page";
+    //// end
     
-    // public accessors
+    //// public accessors
+    //* <method name="get_EncodedTitle" modifiers="public"
+    //* returnType="string">
+    //* Gets HTML encoded page title 
+    //* </method>
     function get_EncodedTitle()
     {
        return htmlentities($this->_title);
     }
     
+    //* <method name="get_Title" modifiers="public"
+    //* returnType="string">
+    //* Gets page title 
+    //* </method>
     function get_Title()
     {
        return $this->_title;
     }
     
+    //* <method name="set_Title" modifiers="public"
+    //* returnType="void">
+    //* Sets page title
+    //* <parameter name="$title" type="string">
+    //* Title of page
+    //* </parameter>
+    //* </method>
     function set_Title($title)
     {
         $this->_title = trim($title);
     }
     
+    //* <method name="get_Page" modifiers="public"
+    //* returnType="&PageBase">
+    //* Gets page using template 
+    //* </method>
     function &get_Page()
     {
         return $this->_page;
     }
     
+    //* <method name="set_Page" modifiers="public"
+    //* returnType="void">
+    //* Sets page using template
+    //* <parameter name="$page" type="&PageBase">
+    //* Page using template
+    //* </parameter>
+    //* </method>
     function set_Page(&$page)
     {
         $this->_page =& $page;
     }
+    //// end
     
-    
-    // public methods
+    //// public methods
+    //* <method name="Render" modifiers="public"
+    //* returnType="void">
+    //* Renders templated page 
+    //* </method>
     function Render() 
     {
         $this->Initialise();
         $this->RenderContent();        
     }
     
-    // abstract public methods
+    //// abstract public methods
+    //* <method name="Initialise" modifiers="public, abstract"
+    //* returnType="void">
+    //* Initialise template
+    //* </method>
     function Initialise() // abstract
     {
     }
     
+    //* <method name="RenderContent" modifiers="public, abstract"
+    //* returnType="void">
+    //* Render templated content
+    //* </method>
     function RenderContent() // abstract
     {
     }
+    //// end
+    //// end
     
-    // protected methods
+    //// protected methods
+    //* <method name="_RenderPlaceHolder" modifiers="protected"
+    //* returnType="void">
+    //* Render the content for the specified placeholder
+    //* <parameter name="$name" type="string">
+    //* Name of place holder
+    //* </parameter>
+    //* </method>
     function _RenderPlaceHolder($name)
     {
         if ($this->_page)
@@ -61,5 +144,6 @@ class PageTemplateBase // abstract
             }
         }
     }
+    //// end
 }
 ?>
