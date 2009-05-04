344859f31e860236bbc75e46ff7d7c34c2999ee4
Trevor Barnett <mr.ullet@gmail.com>
2009-05-04 16:23:42 +0100

    Updated version, date, copyright and license.
    Now: version 0.4, 04 May 2009, Copyright (C) 2009, GPL 3.

--- framework/ThemeList.php
+++ framework/ThemeList.php
@@ -1,25 +1,23 @@
 <?php
 /*
- *************************************************************************
- * PHPPageTemplate: A PHP page templating system.                        *
- * Version 0.3.1 (05 May 2008)                                           *
- * Copyright (C) 2006-2008 Trevor Barnett                                *
- *                                                                       *
- * This program is free software; you can redistribute it and/or modify  *
- * it under the terms of the GNU General Public License as published by  *
- * the Free Software Foundation; either version 2 of the License, or     *
- * (at your option) any later version.                                   *
- *                                                                       *
- * This program is distributed in the hope that it will be useful,       *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- * GNU General Public License for more details.                          *
- *                                                                       *
- * You should have received a copy of the GNU General Public License     *
- * along with this program; if not, write to the Free Software           *
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
- * USA                                                                   *
- *************************************************************************
+ ***************************************************************************************************
+ * PHPPageTemplate: A PHP page templating system.                                                  *
+ * Version 0.4 (04 May 2009)                                                                       *
+ * Copyright (C) 2009 Trevor Barnett                                                               *
+ *                                                                                                 *
+ * This file is part of PHPPageTemplate.                                                           *                                                      *
+ *                                                                                                 *
+ * PHPPageTemplate is free software: you can redistribute it and/or modify it under the terms of   *
+ * the GNU General Public License as published by the Free Software Foundation, either version 3   *
+ * of the License, or (at your option) any later version.                                          *
+ *                                                                                                 *
+ * PHPPageTemplate is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;    *
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See  *
+ * the GNU General Public License for more details.                                                *                          *
+ *                                                                                                 *
+ * You should have received a copy of the GNU General Public License along with PHPPageTemplate.   *
+ * If not, see <http://www.gnu.org/licenses/>.                                                     *
+ ***************************************************************************************************
  */
  
 require_once "Interfaces.php";
